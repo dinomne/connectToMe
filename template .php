0989ba1ce84491e6e134aa5994376a8d12286936
dinomne <dinomne@gmail.com>
2013-08-04 03:53:52 -0700

    The inital commit
    Currently only the nav bar and the basic home screen are inplemented

---
+++ template .php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="template.css" />
+<title>Home Screen</title>
+</head>
+<body>
+	<nav>
+		<button id="homeB" onclick=location.href='home.html'>homeB</button>	
+		<button id="contactB" onclick=location.href='contacts.html'>contactB</button>	
+		<button id="searchB">searchB</button>	
+		<button id="connectB">connectB</button>	
+		<button id="settingsB">settingsB</button>	
+	</nav>
+
+</body>
+</body>
+</html>
