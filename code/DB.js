30e9e0c772ea8f205499f0bb4bfe6c90231c3189
Dinomne <dinomne@gmail.com>
2013-08-11 00:30:02 +0200

    Finished prototype DB, not tested tomorow query and display. Added websites to my profile, also added some buisness card temes.

--- code/DB.js
+++ code/DB.js
@@ -1,14 +1,73 @@
 function onStart() {
-		const dbName = "Contacts";
+	var name = document.getElementById('name').value;
+	var title = document,getElementById('title').value;
+	var address = document.getElementById('address').value;
+	var email = document,getElementById('email').value;
+	var telephone = document,getElementById('telephone').value;
+	var website = document,getElementById('website').value;
+}\
+window.addEventListener("load", onStart, false);
 
-		var request = indexedDB.open(dbName, 2);
 
-		request.onerror = function(event) {
-  // Handle errors.
-  			return 0;
-		};
-		request.onupgradeneeded = function(event) {
-  			var db = event.target.result;.
-  		}
-  	var objectStore = db.createObjectStore("contacts", { keyPath: "number" });
-  	objectStore.createIndex("name", "name", { unique: false });
+contacts.indexedDB.db = null;
+
+contacts.indexedDB.open = function() {
+  var version = 1;
+  var request = indexedDB.open("contacts", version);
+
+  request.onupgradeneeded = function(e) {
+    var db = e.target.result;
+
+	/* version change
+    // A versionchange transaction is started automatically.
+    e.target.transaction.onerror = html5rocks.indexedDB.onerror;
+
+    if(db.objectStoreNames.contains("contacts")) {
+      db.deleteObjectStore("contacts");
+    }
+
+    var store = db.createObjectStore("contacts",
+      {keyPath: "timeStamp"});
+  };
+	*/
+  request.onsuccess = function(e) {
+    html5rocks.indexedDB.db = e.target.result;
+    html5rocks.indexedDB.getAllTodoItems();
+  };
+
+  request.onerror = html5rocks.indexedDB.onerror;
+};
+
+contacts.indexedDB.addTodo = function(todoText) {
+  var db = html5rocks.indexedDB.db;
+  var trans = db.transaction(["contacts"], "readwrite");
+  var store = trans.objectStore("contacts");
+  var request = store.put({
+    "name": todoText,
+    "title": title,
+    "address":address,
+    "email": email,
+    "telephone":telephone,
+    "website":website
+  });
+
+  request.onsuccess = function(e) {
+    // Re-render all the todo's
+    html5rocks.indexedDB.getAllTodoItems();
+  };
+
+  request.onerror = function(e) {
+    console.log(e.value);
+  };
+};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var store = db.openObjectStore('Friends');
+	var user = store.put({name: 'Eric', gender: 'male', likes: 'html5'});
