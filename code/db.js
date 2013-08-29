6373b65018a8d628f4e738bc769afd3f3c29fc7b
Dinomne <dinomne@gmail.com>
2013-08-29 18:06:16 +0200

    figuring out how to unite all DB

--- code/db.js
+++ code/db.js
@@ -1,49 +1,62 @@
-require(['../code/lawnchair'], Lawnchair({name:'people', record:'person'}, function(people){
+require(['../code/lawnchair'], Lawnchair({ime:'people', record:'person'}, function(people){
 
 	var form;
 	
     // something to save...
     
-    [ 'title', 'address', 'email', 'telephone', 'website', 'name', 'temeNo'].forEach(function(id){
-				id = document.getElementById(id);})
-		
+    				
 
-	
-	form = {
-				'name': name.value,
-				'title': title.value,
-				'address':address.value,
-			 	'email': email.value,
-			 	'telephone': telephone.value,
-			 	'website': website.value,
-			 	'temeNo': temeNo.value};
-	
-	
 	document.getElementById('edit').addEventListener('click', function(){
+		['edit', 'ime', 'title', 'address', 'email', 'telephone', 'website', 'temeNo'].forEach(function(id){
+				var id = document.getElementById(id);})
+	var input = document.getElementsByTagName("input");
+	input += 	document.getElementById('temeNo');
+	form = {
+				'ime': input[0].value,
+				'title': input[1].value,
+				'address':input[2].value,
+			 	'email': input[3].value,
+			 	'telephone': input[4].value,
+			 	'website': input[5].value, 
+			 	'temeNo': input[6].value 
+			 	};
+	
+	
 
-        // refer to the callback param (mapped to `name` in ctor options)
+        // refer to the callback param (mapped to `ime` in ctor options)
         // - also notice the terse callback in the second param 
-        // - it uses the named variable person
-        people.save({key:"me", value:form}, 'console.log("ok")');	
+        // - it uses the imed variable person
+        people.save({key:"me", value:form}, function(obj){ console.log(obj)});	
     }, false);
+   
     
-    
- 	
- 	//FIX!!!!
-	function show(name){
-    people.get(name, function(obj) {
-        if(obj) {
-            console.log("found task");
-            val = obj.value;
-            ['name', 'title', 'address', 'email', 'telephone', 'website'].forEach(function(id){
-            	//dodaj HTML inject
-            	console.log(val[id]);
-            }) 
-        } else {
-            console.log("task not found");
-        		}
-    })}
-  document.getElementById('show').addEventListener('click', show("me"), false);
+ 	id = "me";
+ 	function showProfile(id){
+ 		var tags = ['ime', 'title', 'address', 'email', 'telephone', 'website'];
+	
+	
+	
+	
+	tags.forEach(function(name){
+		
+		people.get(id, function(obj) {
+			if(obj) {
+           	 val = obj.value;
+            console.log(val);
+          /*  var p = document.getElementsByTagName("p");
+				for(var i = 0; i < p.length; i++){
+   					//do something to each div like
+   						p[i].innerHTML = val[tags[i]]; 
+   					}
+            */
+            }else {
+            	console.log("task not found");}
+        });});
+        
+        
+   
+ 	}
+	
   }))
   
   
--- code/showProfile.js
+++ code/showProfile.js
@@ -1,25 +1,28 @@
-require(['../code/lawnchair'], Lawnchair({name:'people', record:'person'},function OnLoad (people) {
+
 	
-	var tags = ['name', 'title', 'address', 'email', 'telephone', 'website'];
-	
-	var red = document.getElementById("red");
+	var tags = ['ime', 'title', 'address', 'email', 'telephone', 'website'];
 	
 	
+	id = "me";
 	
+	tags.forEach(function(name){
 		
 		people.get(id, function(obj) {
 			if(obj) {
            	 val = obj.value;
-            var p = document.getElementsByTagName("p");
+            console.log(val);
+          /*  var p = document.getElementsByTagName("p");
 				for(var i = 0; i < p.length; i++){
    					//do something to each div like
-   						p[i].innerHTML = val[tags[i]];
+   						p[i].innerHTML = val[tags[i]]; 
    					}
-            
+            */
             }else {
             	console.log("task not found");}
-        });
-        }))
+        });});
+        
+        
+   
         
         
       
