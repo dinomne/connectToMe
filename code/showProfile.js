d18573691f41d8b1c9c126eb52b8e0f5cd8902e9
Dinomne <dinomne@gmail.com>
2013-08-29 18:06:46 +0200

    unsaved file

--- code/showProfile.js
+++ code/showProfile.js
@@ -1,28 +1,6 @@
 
 	
-	var tags = ['ime', 'title', 'address', 'email', 'telephone', 'website'];
 	
-	
-	id = "me";
-	
-	tags.forEach(function(name){
-		
-		people.get(id, function(obj) {
-			if(obj) {
-           	 val = obj.value;
-            console.log(val);
-          /*  var p = document.getElementsByTagName("p");
-				for(var i = 0; i < p.length; i++){
-   					//do something to each div like
-   						p[i].innerHTML = val[tags[i]]; 
-   					}
-            */
-            }else {
-            	console.log("task not found");}
-        });});
-        
-        
-   
         
         
       
