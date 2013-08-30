b55ef0177f58bbe03a1377774b7ad3267ed7ef23
Dinomne <dinomne@gmail.com>
2013-08-30 18:21:22 +0200

    fixed the DB, show profile works. Added a different mechanism for caling the DB, delited excess files, resized the images

--- code/db.js
+++ code/db.js
@@ -1,16 +1,18 @@
-require(['../code/lawnchair'], Lawnchair({ime:'people', record:'person'}, function(people){
+people = Lawnchair({name:'people', record:'person'},function() { console.log("db created");});
 
+
+function Database(people){
 	var form;
 	
     // something to save...
     
-    				
+   		
 
 	document.getElementById('edit').addEventListener('click', function(){
-		['edit', 'ime', 'title', 'address', 'email', 'telephone', 'website', 'temeNo'].forEach(function(id){
-				var id = document.getElementById(id);})
+		
 	var input = document.getElementsByTagName("input");
-	input += 	document.getElementById('temeNo');
+	var teme = 	document.getElementById('temeNo').value;
+	
 	form = {
 				'ime': input[0].value,
 				'title': input[1].value,
@@ -18,45 +20,57 @@ require(['../code/lawnchair'], Lawnchair({ime:'people', record:'person'}, functi
 			 	'email': input[3].value,
 			 	'telephone': input[4].value,
 			 	'website': input[5].value, 
-			 	'temeNo': input[6].value 
+			 	'temeNo': teme 
 			 	};
-	
 	
 
         // refer to the callback param (mapped to `ime` in ctor options)
         // - also notice the terse callback in the second param 
         // - it uses the imed variable person
-        people.save({key:"me", value:form}, function(obj){ console.log(obj)});	
-    }, false);
-   
+    people.save({key:"me", value:form}, function(obj){ console.log(obj)});	
     
- 	id = "me";
- 	function showProfile(id){
+ 
+  })}
+ 
+ 
+ window.onload = function(){
+ 	if (document.getElementById("My Profile").innerHTML==="My Profile")
+ 	{
+ 		showProfile(id);
+ 		console.log("show profile");
+ 	}else if (document.getElementById("My Profile").innerHTML==="Edit My Profile"){
+ 		Database(people);
+ 		console.log("edit profile");
+ 	}else{
+ 		console.log("no function for this");
+ 	}
+ };
+  
+
+
+function showProfile(id){
  		var tags = ['ime', 'title', 'address', 'email', 'telephone', 'website'];
-	
-	
-	
-	
-	tags.forEach(function(name){
 		
+	
 		people.get(id, function(obj) {
 			if(obj) {
+           	 
            	 val = obj.value;
-            console.log(val);
-          /*  var p = document.getElementsByTagName("p");
+            
+            var p = document.getElementsByTagName("p");
 				for(var i = 0; i < p.length; i++){
    					//do something to each div like
    						p[i].innerHTML = val[tags[i]]; 
    					}
-            */
+            
+            document.getElementById("temeL").href = '../css/teme/' + obj.value.temeNo+ '.css';
+   			console.log('href="../css/teme/' + obj.value.temeNo+ '.css"');
+            
             }else {
             	console.log("task not found");}
-        });});
-        
-        
-   
- 	}
+        });
+ }		
 	
-  }))
-  
-  
+	
+
+	
